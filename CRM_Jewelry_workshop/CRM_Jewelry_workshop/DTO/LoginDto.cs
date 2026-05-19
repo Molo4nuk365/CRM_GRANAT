c6646f6a55a984fa742c90a9cbf555a8aa044684
Филановская <filanosvkaya.marianne@gmail.com>
2026-05-20 00:32:54 +0300

    Models + DTO

---
+++ CRM_Jewelry_workshop/CRM_Jewelry_workshop/DTO/LoginDto.cs
@@ -0,0 +1,6 @@
+﻿namespace CRM_Jewelry_workshop.DTO
+{
+    public class LoginDto
+    {
+    }
+}
---
+++ CRM_Jewelry_workshop/CRM_Jewelry_workshop/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using SharpDX.Direct3D9;
+using StackExchange.Redis;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_Jewelry_workshop.Models;
+
+public class Position
+{
+    [Key]
+    public int PositionId { get; set; }
+
+    public int OrderId { get; set; }
+    [ForeignKey(nameof(OrderId))]
+    public Order? Order { get; set; }
+
+    public int MaterialId { get; set; }
+    [ForeignKey(nameof(MaterialId))]
+    public Material? Material { get; set; }
+
+    public decimal Price { get; set; } // цена материала на момент заказа
+    public decimal Quantity { get; set; }
+}
